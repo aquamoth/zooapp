6d5959eb4e9a97ea32ce5882243ca6d3f430a7af
Mattias Åslund <mattias.aslund@shoppa.com>
2020-07-06 19:52:32 +0200

    Map page done

---
+++ Assets/Scripts/PageAwakeState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PageAwakeState : MonoBehaviour
+{
+    [SerializeField] private bool startActive;
+
+    private void Start()
+    {
+        this.gameObject.SetActive(startActive);
+    }
+}
